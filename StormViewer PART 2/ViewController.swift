69c8afeba18dcfc24883dafe4608f3c5f9d1aa8e
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-06-06 23:55:04 +0300

    created funcs for storing and displaying names of pics

--- StormViewer PART 2/ViewController.swift
+++ StormViewer PART 2/ViewController.swift
@@ -7,11 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var images: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath
+        let pictures = try! fm.contentsOfDirectory(atPath: path!)
+        
+        for picture in pictures{
+            if picture.hasPrefix("nssl"){
+                
+                images.append(picture)
+            }
+        }
+        print(images)
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Image", for: indexPath)
+        
+        cell.textLabel?.text = images[indexPath.row]
+        return cell
     }
 
 
