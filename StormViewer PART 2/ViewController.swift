0be61d7c2d43b09a8349484ba84a24ce1fc72392
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-06-06 23:27:40 +0300

    Initial Commit

---
+++ StormViewer PART 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StormViewer PART 2
+//
+//  Created by Nikita  on 6/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
